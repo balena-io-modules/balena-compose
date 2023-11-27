3d98ccbc099e290eb4d0ee08371246afc49f1073
Thodoris Greasidis <thgreasi@gmail.com>
2023-11-27 14:46:11 +0200

    .mocharc: Remove the "exit: true" workaround for hanging on completion on node20
    Change-type: patch

--- .mocharc.js
+++ .mocharc.js
@@ -1,7 +1,4 @@
 module.exports = {
   timeout: 25000,
   spec: 'test/**/*.spec.ts',
-  // TODO: This shouldn't be necessary, but the tests on node20 hang, while they pass on node 16 & 18.
-  // `leaked-handles` showed an unclosed docker deamon connection.
-  exit: true,
 };
